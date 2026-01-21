c67872993dc9be6fbe901574273ddcd75d65ddab
Petr0611 <Ciolacpp@gmail.com>
2026-01-21 17:50:08 +0100

    separation of backend from frontend bagfix

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ const nodemailer = require("nodemailer");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.use(cors());
 app.use(express.json());
